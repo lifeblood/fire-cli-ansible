35e315bbbfd409f601c944560da363a759f90423
danny <danny@wgame200.com>
2019-12-28 19:58:30 +0800

    first commit

--- app/models/ansible.py
+++ app/models/ansible.py
@@ -5,6 +5,9 @@ class Ansible(object):
     def __init__(self):
         pass
 
+    def get_hosts(self):
+        pass
+
     def bootstrap(self, role, json_parameter):
         cmd = ['ansible-playbook playbooks/' + role + '.yml -e ' + json_parameter]
         app.utils().subprocess_check_call(*cmd)
